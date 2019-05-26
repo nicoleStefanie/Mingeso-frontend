0b5deffa20d12ac190e88e86985c9158bb1d381c
Unknown <vicente.brevis@usach.cl>
2019-05-26 17:19:58 -0400

    Corrigiendo habitaciones porque alguien olvido hacer pull

--- src/pages/AgregarHabitaciones.vue
+++ src/pages/AgregarHabitaciones.vue
@@ -1,40 +1,50 @@
 <template>
-  <form>
-    <md-card>
-      <md-card-header :data-background-color="dataBackgroundColor">
-        <h4 class="title">Agregar Habitación</h4>
-        <p class="category">Completar los campos</p>
-      </md-card-header>
-      <md-card-content>
-        <div class="md-layout">
-          <div class="md-layout-item md-small-size-100 md-size-50">
-            <md-field>
-              <label>Tipo</label>
-              <md-input v-model="tipo" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-50">
-            <md-field>
-              <label>Estado</label>
-              <md-input v-model="estado" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-30">
-            <md-field>
-              <label>Precio Noche</label>
-                <md-input v-model="precio" type="number" min="100000"></md-input>
-                <md-icon>attach_money</md-icon>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-size-100 text-right">
-            <md-button class="md-raised md-success" :href="'#/habitaciones'">Cancelar</md-button>
-            &nbsp; &nbsp; 
-            <md-button class="md-raised md-success"  @click="agregarHabitacion">Agregar Habitación</md-button>
-          </div>
-        </div>
-      </md-card-content>
-    </md-card>
-  </form>
+    <form>
+        <md-card>
+            <md-card-header :data-background-color="dataBackgroundColor">
+                <h4 class="title">Agregar Habitación</h4>
+                <p class="category">Completar los campos</p>
+            </md-card-header>
+            <md-card-content>
+                <div class="md-layout">
+                    <div class="md-layout-item md-small-size-100 md-size-50">
+                        <md-field>
+                            <label>N° de habitacion</label>
+                            <md-input v-model="nroHabitacion" type="text"></md-input>
+                        </md-field>
+                    </div>
+                    <div class="md-layout-item md-small-size-100 md-size-50">
+                        <md-field>
+                            <label>Tipo</label>
+                            <md-input v-model="tipo" type="text"></md-input>
+                        </md-field>
+                    </div>
+                    <div class="md-layout-item md-small-size-100 md-size-50">
+                        <md-field>
+                            <label>Capacidad niños</label>
+                            <md-input v-model="capacidadNinos" type="text"></md-input>
+                        </md-field>
+                    </div>
+                    <div class="md-layout-item md-small-size-100 md-size-50">
+                        <md-field>
+                            <label>Capacidad Adultos</label>
+                            <md-input v-model="capacidadAdultos" type="text"></md-input>
+                        </md-field>
+                    </div>
+                    <div class="md-layout-item md-small-size-100 md-size-30">
+                        <md-field>
+                            <label>Precio Noche </label>
+                            <md-input v-model="precioNoche"></md-input>
+                            <md-icon>attach_money</md-icon>
+                        </md-field>
+                    </div>
+                    <div class="md-layout-item md-size-100 text-right">
+                        <md-button class="md-raised md-success" @click="agregarHabitacion" >Agregar Habitación</md-button>
+                    </div>
+                </div>
+            </md-card-content>
+        </md-card>
+    </form>
 </template>
 
 <script>
@@ -61,8 +71,8 @@ export default {
       var url = localhost + '/habitaciones/create';
       axios.post(url, {
 
-        nroHabitacion : this.nroHabitacion,
         tipo : this.tipo,
+        nroHabitacion : this.nroHabitacion,
         capacidadNinos : this.capacidadNinos,
         capacidadAdultos: this.capacidadAdultos,
         precioNoche: this.precioNoche

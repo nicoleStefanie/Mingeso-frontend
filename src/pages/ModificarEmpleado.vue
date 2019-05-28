5c04cb86612a3e4f6470a3666cf0e0b16c0e4399
Unknown <liliana.huallcca@usach.cl>
2019-05-28 10:31:19 -0400

    Segunda prueba arreglo conexion de back y front

--- src/pages/ModificarEmpleado.vue
+++ src/pages/ModificarEmpleado.vue
@@ -43,7 +43,7 @@
 
 <script>
 import axios from 'axios';
-const localhost = 'http://localhost:8060/backend';
+const localhost = 'http://159.203.94.72:8060/backend';
 export default {
   name : 'modificarEmpleado',
 

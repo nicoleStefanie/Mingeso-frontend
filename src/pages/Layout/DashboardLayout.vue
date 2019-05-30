a736b883d0b3d7aca5cd98572bb5f68d58613857
Unknown <paulina.aburto@usach.cl>
2019-05-30 15:54:43 -0400

    Boton Rack

--- src/pages/Layout/DashboardLayout.vue
+++ src/pages/Layout/DashboardLayout.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="wrapper" :class="{'nav-open': $sidebar.showSidebar}">
-    <notifications></notifications>
-
-      <sidebar-link to="/rack">
+    <notifications></notifications>      
+    <side-bar>
       <mobile-menu slot="content"></mobile-menu>
+   
+      <sidebar-link to="/rack">
         <md-icon>notes</md-icon>
         <p>Rack</p>
       </sidebar-link>
-      
-    <side-bar>
+
       <sidebar-link to="/reservas">
         <md-icon>assignment</md-icon>
         <p>Reservas</p>

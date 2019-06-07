2318d8d673818d4e358fe45db50324cffd4ecb9e
josemellado <jose.mellado@usach.cl>
2019-06-07 11:14:20 -0400

    Avance checkin

--- src/pages/Checkin.vue
+++ src/pages/Checkin.vue
@@ -17,7 +17,7 @@
             </div>
             <br><br>
             <div class="md-layout-item md-small-size-100 md-size-100 text-center">
-              <md-button class="md-raised md-success" style="width: 60%; font-size:20px" @click="validar">Comenzar Check-in</md-button>
+              <md-button class="md-raised md-success" style="width: 60%; font-size:20px" @click="enviar">Comenzar Check-in</md-button>
             </div>
         </div>
       </md-card-content>
@@ -34,15 +34,48 @@
     },
     data(){
       return{
-        vatError1,
-        vatErrorMsg1,
-        vatError2,
-        vatErrorMsg2,
+        vatError1: '',
+        vatErrorMsg1: '',
+        vatError2: '',
+        vatErrorMsg2: '',
+        apellidoCliente: '',
+        codigoReserva: '',
       }
     },
     methods: {
-      refresh(){
-        location.reload(true);
+
+      validarCodigo:function(){
+        if(/^\d*$/.test(this.codigoReserva)){
+            this.vatError1 = false;
+            this.vatErrorMsg1 = null;
+        }
+        else{
+          this.vatError1 = true;
+          this.vatErrorMsg1 = "Ingrese un valor númerico."
+        }
+      },
+      validarApellido:function(){
+        if(/^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/g.test(this.apellidoCliente)) {
+          this.vatError2 = true;
+          this.vatErrorMsg2 = null;
+          return true }
+        else{
+          this.vatError2 = true;
+          this.vatErrorMsg2 = "Ingrese un apellido válido.";
+        }
+      },
+      enviar(){
+        var url = api + '/registro/checkin';
+        axios.post(url, {
+          codigoReserva:this.codigoReserva,
+          representante:this.apellidoCliente,
+        }).then(response => {
+          alert(response.data[0].message);
+          console.log(response.data.message);
+          if(response.data[0].message == 'OK' || true){
+            location.href = "http://159.203.94.72/#/checkin/registro";
+          }
+          })
       },
   },
   };

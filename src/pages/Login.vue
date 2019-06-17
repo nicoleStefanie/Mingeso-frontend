49be5042b7d069f29525410fb6869aab1510bad1
Edgar Blau <edgar.blau@usach.cl>
2019-06-17 17:58:10 -0400

    Mensajes de error de login agregados.

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -39,28 +39,78 @@ export default{
   methods: {
     async inicio () {
       let url = 'http://159.203.94.72:8060/backend/usuarios/login'
-      await axios.post(url, {
-        email: this.email,
-        password: this.password
-      }, { useCredentails: true }).then((data) => {
-        let loginData = data['data'][0]
-        if (loginData['status'] === 200) {
-          localStorage.setItem('role', loginData['role'])
-          localStorage.setItem('user_id', loginData['id'])
-          localStorage.setItem('login', loginData['login'])
-          localStorage.setItem('name', loginData['name'])
-          localStorage.setItem('email', loginData['email'])
-          this.updateComponents(loginData['name'], loginData['role'])
-          this.$router.push('Rack')
-        }
-        else {
-          alert(loginData['message'])
-        }
-      })
+      let inputData = this.checkData(this.email, this.password);
+      if (inputData['result']) {
+        await axios.post(url, {
+          email: this.email,
+          password: this.password
+        }, { useCredentails: true }).then((data) => {
+          let loginData = data['data'][0]
+          if (loginData['status'] === 200) {
+            localStorage.setItem('role', loginData['role'])
+            localStorage.setItem('user_id', loginData['id'])
+            localStorage.setItem('login', loginData['login'])
+            localStorage.setItem('name', loginData['name'])
+            localStorage.setItem('email', loginData['email'])
+            this.updateComponents(loginData['name'], loginData['role'])
+            this.$router.push('Rack')
+          }
+          else {
+            alert(loginData['message'])
+          }
+        })
+      }
+      else {
+        alert(inputData['error'])
+      }
     },
     updateComponents (name, role) {
       this.$root.$emit('doLogin', name)
       this.$root.$emit('setRole', role)
+    },
+    checkData (email, password) {
+      let errorMsg = 'Error(es) detectado(s):\r\n'
+      if (email && password){
+        if(email.length < 5 && password.length < 5) {
+          errorMsg = errorMsg + '- Ingrese un email correcto.\r\n- Ingrese una contrasena correcta.'
+          return {error: errorMsg, result: false}
+        }
+        else if (email.length > 5 && password.length < 5 ) {
+          errorMsg = errorMsg + '- Ingrese una contrasena correcta.'
+          return {error: errorMsg, result: false}
+        }
+        else if (email.length < 5 && password.length > 5) {
+          errorMsg = errorMsg + '- Ingrese un email correcto.'
+          return {error: errorMsg, result: false}
+        }
+        else {
+          return {error: errorMsg, result: true}
+        }
+      }
+      else if (email && !password) {
+        if (email.length < 5) {
+          errorMsg = errorMsg + '- Ingrese un email correcto.\r\n- Ingrese una contrasena correcta.'
+          return {error: errorMsg, result: false}
+        }
+        else {
+          errorMsg = errorMsg + '- Ingrese una contrasena correcta.'
+          return {error: errorMsg, result: false}
+        }
+      }
+      else if (!email && password) {
+        if (password.length < 5) {
+          errorMsg = errorMsg + '- Ingrese un email correcto.\r\n- Ingrese una contrasena correcta.'
+          return {error: errorMsg, result: false}
+        }
+        else {
+          errorMsg = errorMsg + '- Ingrese un email correcto.'
+          return {error: errorMsg, result: false}
+        }
+      }
+      else {
+        errorMsg = errorMsg + '- Ingrese un email correcto.\r\n- Ingrese una contrasena correcta.'
+        return {error: errorMsg, result: false}
+      }
     }
   },
   mounted () {

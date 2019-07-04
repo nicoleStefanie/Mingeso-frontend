de3bbdd26134ccc7930e878e4a8cca316f44292a
Edgar Blau <edgar.blau@usach.cl>
2019-07-04 16:31:43 -0400

    F

--- src/pages/Rack.vue
+++ src/pages/Rack.vue
@@ -18,7 +18,7 @@
 var hoy = new Date();
 import { existsSync } from 'fs';
 import axios from 'axios';
-const localhost = 'http://127.0.0.1:8060/backend';
+const localhost = 'http://159.203.94.72:8060/backend';
 export default {
         data(){
             return{
